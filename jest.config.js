cc29f3235a1cd1374abcea7256b5f0cee8734450
SPACIALTRIGGER <spacialarmy@gmail.com>
2026-07-25 00:47:31 +0530

    Add Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+{
+  "testEnvironment": "node",
+  "coverageDirectory": "coverage",
+  "collectCoverageFrom": [
+    "src/**/*.js",
+    "!src/**/*.test.js",
+    "!src/**/index.js"
+  ],
+  "testMatch": [
+    "**/tests/**/*.test.js"
+  ],
+  "verbose": true
+}
